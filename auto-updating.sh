6260ea8198f9376eb5c712cde7697b4a67abb984
PingalPie <lscience611@gmail.com>
2023-02-04 11:26:33 +0530

    idk

--- auto-updating.sh
+++ auto-updating.sh
@@ -1,6 +1,6 @@
-echo "Building"
+echo "Building..."
 zola build > /dev/null
-echo "Removing previous build"
+echo "Removing previous build..."
 ssh pingalpie@pingalpie.tildevarsh.in 'rm -rf public_html'
 echo "Uploading..."
 rsync -az public/* pingalpie@pingalpie.tildevarsh.in:~/public_html
