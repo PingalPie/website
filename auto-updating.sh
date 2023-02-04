ed98b67222f2e686398da47e56384df5c0d2371d
PingalPie <lscience611@gmail.com>
2023-02-04 11:27:56 +0530

    removed auto-updating

--- auto-updating.sh
+++
@@ -1,7 +0,0 @@
-echo "Building..."
-zola build > /dev/null
-echo "Removing previous build..."
-ssh pingalpie@pingalpie.tildevarsh.in 'rm -rf public_html'
-echo "Uploading..."
-rsync -az public/* pingalpie@pingalpie.tildevarsh.in:~/public_html
-rm -rf public
