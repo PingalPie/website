14b1733ccb72c218c49550893b82ab91fa8c37d6
PingalPie <pingalpie@proton.me>
2026-01-19 12:54:31 +0530

    copied atom from ipho-unofficial.org

--- static/js/main.js
+++ static/js/main.js
@@ -34,3 +34,55 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 });
+// Homepage animation
+document.addEventListener("DOMContentLoaded", () => {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var a = 250;
+    var ball_r = 8;
+    var ball_w = 90;
+    var ball_h = 27;
+
+    function rect(x, y, fill, stroke) {
+	ctx.save();
+	ctx.fillStyle = fill;
+	ctx.strokeStyle = stroke;
+	ctx.beginPath();
+	ctx.rect(x, y, a * 0.6, a * 0.6);
+	ctx.fill();
+	ctx.stroke();
+	ctx.clip();
+	ctx.fillStyle = stroke;
+	ctx.rotate(new Date().getTime() * .0001 % (2 * Math.PI));
+	for (var i = 0; i < 3; i++) {
+	    ctx.beginPath();
+	    ctx.save();
+	    ctx.scale(1, ball_h / ball_w);
+	    ctx.arc(0, 0, ball_w, 0, 2 * Math.PI);
+	    ctx.stroke();
+	    ctx.restore();
+	    var ba = new Date().getTime() * 0.0023 * (1 + i * 0.09);
+	    var ball_x = ball_w * Math.sin(ba);
+	    var ball_y = ball_h * Math.cos(ba);
+	    ctx.beginPath();
+	    ctx.arc(ball_x, ball_y, ball_r, 0, 2 * Math.PI);
+	    ctx.fill();
+	    ctx.rotate(Math.PI / 3);
+	}
+	ctx.restore();
+    }
+
+    function render() {
+	requestAnimationFrame(render);
+
+	ctx.save();
+	ctx.translate(canvas.width / 2, canvas.height / 2);
+
+	rect(-a * 0.10, -a * 0.32, "#388be2", "#9038e2");
+	rect(-a * 0.32, -a * 0.10, "#9090ff", "#f400fc");
+	rect(-a * 0.50, -a * 0.50, "#27bab5", "#00fcf4");
+
+	ctx.restore();
+    }
+    render();
+})
